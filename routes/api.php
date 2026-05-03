cc60de0a44507060664876f51d1f70f0aff71611
mostafaEdreas <mostafa.fahmy205@gmail.com>
2026-05-03 13:54:49 +0300

    fix confilects names between api and nad web

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\Api\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('orders', OrderController::class)
-->middleware('prevent-duplicate-request')
-->only(['store', 'show', 'index']);
+Route::name('api.')->group(function () {
+    Route::apiResource('orders', OrderController::class)
+        ->middleware('prevent-duplicate-request')
+        ->only(['store', 'show', 'index']);
+});
 
 Route::prefix('orders/{order}')->controller(OrderController::class)->group(function () {
     Route::post('/processing', 'processing')->name('api.orders.processing');
